9db3867cb54b8d539f7bff7e5a74e085b1675168
Justin Choi <yjustin2004@gmail.com>
2026-07-05 02:23:37 -0400

    Done configuring! Should be good to go. Make sure to use 'ng deploy' when updating!

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura'; // Import your chosen theme preset
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes, withHashLocation()),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
   ]
 };
